c41fb6d85c8e18315ef9d85a0f34c85a7e78baca
ericsalesdeandrade <sdaeric19@gmail.com>
2024-05-28 09:28:33 +0100

    More tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,9 @@
+import pytest
 import re
 from playwright.sync_api import Page, expect
 
 
+@pytest.mark.basic
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
 
@@ -9,11 +11,48 @@ def test_has_title(page: Page):
     expect(page).to_have_title(re.compile("Playwright"))
 
 
-# def test_get_started_link(page: Page):
-#     page.goto("https://playwright.dev/")
+@pytest.mark.basic
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.dev/")
 
-#     # Click the get started link.
-#     page.get_by_role("link", name="Get started").click()
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
 
-#     # Expects page to have a heading with the name of Installation.
-#     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+
+@pytest.mark.search
+def test_duckduckgo_search(page: Page):
+    # Go to the DuckDuckGo homepage.
+    page.goto("https://duckduckgo.com/")
+
+    # Type "Pytest with Eric" into the search box and press Enter.
+    page.get_by_placeholder("Search without being tracked").fill("Pytest with Eric")
+
+    # Press Enter to submit the search.
+    page.locator('button[aria-label="Search"]').click()
+
+    # Get the first search result using get_by_role
+    first_result = page.get_by_role("link", name=re.compile("Pytest")).first
+
+    # Expect the first result to contain the text "Pytest"
+    expect(first_result).to_have_text(re.compile("Pytest With Eric"))
+
+
+@pytest.mark.login
+def test_login(page: Page):
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+
+    # Fill in the login form
+    page.get_by_label("username").fill("student")
+    page.get_by_label("password").fill("Password123")
+
+    # Submit the form
+    page.get_by_role("button", name="Submit").click()
+
+    # Look for "Congratulations Message" text
+    expect(page.get_by_text(re.compile("Congratulations"))).to_be_visible()
+
+    # Look for a Logout Button
+    expect(page.get_by_text(re.compile("Log out"))).to_be_visible()
